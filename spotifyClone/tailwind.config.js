e7bda3ffdd2b0b779f1fc997fcab94d2d1590a14
Aman Sharma <mail.aman@protonmail.com>
2023-05-27 17:36:42 +0400

    Dashboard UI scaffolding added

--- spotifyClone/tailwind.config.js
+++ spotifyClone/tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     extend: {
       colors:{
         "spGreen": "#1DB954",
-        "spBlack": "#191414"
+        "spBlack": "#191414",
+        "hoverLighten": "rgb(45, 40, 40)",
+        "superLightBlack": "rgb(60, 50, 50)"
       }
     },
   },
